4904a2906894d96281a8467aee9fa521704e3238
NaileNesrullayevaa <nailenesrullayeva536@gmail.com>
2024-11-17 07:42:54 -0800

    main

---
+++ main.js
@@ -0,0 +1,81 @@
+// const arr=['kitab','defter','qelem','pozan']
+// arr.splice(2,1,'jurnal','karandas')
+// console.log(arr)
+
+// ------------------------------------------------
+
+// const arr=[2,5,32,12,6]
+// for(let i=0;i<arr.length;i++){
+//     if(arr[i]%2==0){
+//         console.log('cutdur');
+        
+//     }
+//     else{
+//         console.log('tekdir')
+//     }
+// }
+
+// -------------------------------------------------
+// const arr=[-23,5,34,3,-3,0]
+
+// for(let i=0;i<arr.length;i++){
+//     if(arr[i]>=0){
+//        const arr2=arr[i]*2
+//         // console.log(arr2.sort());
+        
+        
+//     }
+    
+// }
+
+// ----------------------------------------------------
+
+// let word="update"
+// switch(word){
+//     case 'save':
+//         console.log('yadda saxlandi');
+//         break;
+//     case 'cancel':
+//         console.log('cixildi');
+//         break;
+//     case 'update':
+//         console.log('deyisiklikler yadda saxlanildi')
+//         break;
+// default:
+//     console.log('qebul edilmedi')
+// }
+
+
+// ---------------------------------------------------------
+
+// let arr=[4,6,3,2,5,7]
+// for(let i=0;i<=arr.length;i++){
+//     let result=(arr[i]%2==0)?console.log("cut"):console.log("tek");
+    
+// }
+
+// --------------------------------------------------------------------
+// let word='abcfdeg'
+// console.log(Array.from(word));
+
+// ----------------------------------------------------------------------
+// const obj1={cloud:'bulud',book:'kitab',juice:'meyve siresi'}
+// const obj2={phone:'telefon',window:'pencere'}
+// result=Object.assign(obj1,obj2)
+// for(let i in result){
+//     console.log(result[i])
+// }
+
+// -----------------------------------------------------------------------------
+// const arr=['alma','armud','heyva','ananas','erik']
+// let say=0
+// let cem=0
+// for(let i=0;i<arr.length;i++){
+//     if('a'.includes(arr[i])){
+//         say++
+//         cem+=say
+
+//     }
+//     console.log(arr[say]);
+
+// }
